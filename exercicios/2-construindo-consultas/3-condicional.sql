c84db64cd96835f4ee648076d9ee9e5452c5b635
Vjs2k24 <vinicius.januario-silva@basf.com>
2024-07-26 14:34:09 +0000

    EX2_3

--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,2 +1,12 @@
 -- Escreva uma consulta que retorne o nome por extenso dos estados brasileiros
-
+SELECT DISTINCT
+Country AS Pais, State AS Estado,
+CASE
+  WHEN State = 'SP' THEN 'São Paulo'
+  WHEN State = 'RJ' THEN 'Rio de Janeiro'
+  ELSE 'Estado Desconhecido'
+END AS estado
+FROM
+customers
+WHERE
+Country LIKE 'Brazil';
