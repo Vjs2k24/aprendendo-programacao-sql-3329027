f5cbbe1ceeb2968e94e2493852ecb9b0dbba8d28
Vjs2k24 <vinicius.januario-silva@basf.com>
2024-07-26 13:49:06 +0000

    EX_2_3

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,10 @@
 -- Insira 3 classes salariais na tabela salario_classe
-
-
+INSERT INTO salario_funcionarios (id_salario, nivel, salario)
+VALUES
+(5, 'Médico', 5000),
+(6, 'Enfermeiro', 3200),
+(7, 'Vendedor', 2500);
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+INSERT INTO salario_funcionarios (nivel, salario)
+VALUES
+('Reservista', 2000);
